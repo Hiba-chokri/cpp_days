bea1dc5b1df40962c11471fd2eaa2a3da925c350
Hiba Chokri <hichokri@e1r2p8.1337.ma>
2025-11-12 23:23:28 +0100

    ex00 and ex01 done

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,7 +1,13 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-
+template <typename T, typename Func>
+void iter(T* arr, const int len, Func f)
+{
+    for (int i = 0; i < len; ++i) {
+        f(arr[i]);
+    }
+}
 
 
 #endif 
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,58 @@
+#include "iter.hpp"
+#include <iostream>
+#include <string.h>
+
+
+void incrementInt(int& x) {
+    ++x;
+}
+
+void printInt(int const &x) {
+    std::cout << x << " ";
+}
+
+void printString(std::string const &str) {
+    std::cout << str << " ";
+}
+
+void printDouble(double const &d) {
+    std::cout << d << " ";
+}
+
+int main()
+{
+    int values[] = {5, 10, 15, 20, 25};
+    size_t valuesSize = 5;
+     std::cout << "  Original: ";
+    iter(values, valuesSize, printInt);
+    std::cout << std::endl;
+    iter(values, valuesSize, incrementInt);
+    std::cout << "  +1:       ";
+    iter(values, valuesSize, printInt);
+    std::cout << std::endl << std::endl;
+
+    std::cout << " Double array" << std::endl;
+    double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    size_t doubleSize = 5;
+    
+    iter(doubleArray, doubleSize, printDouble);
+    std::cout << std::endl;
+
+    std::cout << "TEST 3: String array" << std::endl;
+    std::string stringArray[] = {"Hello", "World", "C++", "Templates"};
+    size_t stringSize = 4;
+    
+    std::cout << "  Strings: ";
+    iter(stringArray, stringSize, printString);
+    std::cout << std::endl << std::endl;
+
+
+    const int constArray[] = {100, 200, 300, 400, 500};
+    size_t constSize = 5;
+    std::cout << "  Const array: ";
+    iter(constArray, constSize, printInt);
+    std::cout << std::endl;
+
+    // This would NOT compile (trying to modify const array):
+    //  iter(constArray, constSize, incrementInt);
+}
