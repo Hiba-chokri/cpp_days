dc298725b1a6f46a71a51c5385e235854945edf7
Hiba-chokri <hebachokri7@gmail.com>
2025-11-27 14:13:28 +0100

    everything is done

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -33,6 +33,13 @@ int main() {
     catch (std::exception &e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
-
+//     Span sp = Span(5);
+// sp.addNumber(6);
+// sp.addNumber(3);
+// sp.addNumber(17);
+// sp.addNumber(9);
+// sp.addNumber(11);
+// std::cout << sp.shortestSpan() << std::endl;
+// std::cout << sp.longestSpan() << std::endl;
     return 0;
 }
