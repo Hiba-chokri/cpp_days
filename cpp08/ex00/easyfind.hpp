f2919fcc35362708551d8772bbe27c458c2a4fd8
Hiba Chokri <hichokri@c4r10p9.1337.ma>
2025-12-03 21:08:37 +0100

    updated ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -8,14 +8,11 @@
 template<typename T>
 typename T::iterator easyfind(T& container , int value)
 {
-    for(typename T::iterator it = container.begin(); it != container.end(); it++)
-    {
-        if (*it == value)
-        {
-            return it;
-        }
-    }
-    throw std::runtime_error("Value not found");
+    
+    typename T::iterator it = std::find(container.begin(), it != container.end(), value);
+    if (it == container.end())
+        throw std::runtime_error("Value not found");
+    return it;
 }
 
 
