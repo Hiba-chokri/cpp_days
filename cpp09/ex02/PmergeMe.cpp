c70d744a385e03c4f1209506c977bac2c3d75e9e
Hiba Chokri <hichokri@c4r1p8.1337.ma>
2026-01-02 17:42:20 +0100

    parsing done

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -14,5 +14,30 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &obj)
     {
         
     }
-    return *this;
+    return *
+    this;
+}
+
+bool PmergeMe::isValidNumber(const std::string &str) {
+    if (str.empty()) 
+        return false;
+    for (size_t i = 0; i < str.size(); ++i) {
+        if (!isdigit(str[i]))
+            return false;
+    }
+    return true;
+}
+
+void PmergeMe::validateInput(int argc, char **argv) {
+    for (int i = 1; i < argc; ++i) {
+        std::string arg(argv[i]);
+
+        if (!isValidNumber(arg))
+            throw std::invalid_argument("Invalid number: " + arg);
+        int num;
+        std::stringstream ss(arg);
+        ss >> num;
+        _vectorData.push_back(num);
+        _dequeData.push_back(num);
+    }
 }
